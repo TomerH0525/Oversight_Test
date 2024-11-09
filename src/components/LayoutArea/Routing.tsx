40e39535014cd59fa9439a90b72ce9aa66121fa1
Tomer2H <tomer2b0525@gmail.com>
2024-11-09 13:17:17 +0200

    added a fallback 404NotFound component page to router

--- src/components/LayoutArea/Routing.tsx
+++ src/components/LayoutArea/Routing.tsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../HomePage";
+import NotFound404 from "../ErrorPages/NotFound404";
 
 
 function Routing(): JSX.Element {
     return(
-        <div className="Routing">
+        <div className="h-full w-auto">
 
             <Routes>
-                <Route index path="/" element={<HomePage />}/>
+                <Route index path="/" element={<HomePage />} />
 
 
+                <Route path="*" element={<NotFound404/>} />
             </Routes>
 
 
