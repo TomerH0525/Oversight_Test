b3d5c08abec2f497516d6c66289ebf4d6e86c770
Tomer2H <tomer2b0525@gmail.com>
2024-11-12 22:40:50 +0200

    updated User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,19 +1,24 @@
+import ClientType from "./enum/ClientType";
+
 class User {
   userId: number;
   username: string;
   password: string;
-  isLocked: boolean;
+  isLocked?: boolean;
+  clientType: ClientType;
 
   constructor(
     userId: number,
     userName: string,
     password: string,
-    isLocked: boolean
+    isLocked: boolean,
+    clientType: ClientType,
   ) {
     this.userId = userId;
     this.username = userName;
     this.password = password;
     this.isLocked = isLocked;
+    this.clientType = clientType;
   }
 
 }
