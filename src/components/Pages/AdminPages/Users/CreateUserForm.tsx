659bcc1e27a8707af50bda5c2b6f823c5bca2277
Tomer2H <tomer2b0525@gmail.com>
2024-11-12 23:04:24 +0200

    Updated createUserForm to include createLoginForm

--- src/components/Pages/AdminPages/Users/CreateUserForm.tsx
+++ src/components/Pages/AdminPages/Users/CreateUserForm.tsx
@@ -6,30 +6,36 @@ import { FormControl, FormDescription, FormField, Form, FormItem, FormLabel, For
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import authService from "@/services/AuthService";
 
 
-const formSchema = z.object({
+const registerFormSchema = z.object({
     username: z.string().min(4, 'Username need to be atleast 4 charaters'),
     password: z.string().min(8, 'Password must be at least 8 characters'),
   });
 
+  const loginFormSchema = z.object({
+    username: z.string().min(2,'Username is required'),
+    password: z.string().min(2,"Password is required")
+  });
 
-const CreateUserForm = () => {
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+export const CreateUserForm = () => {
+
+    const form = useForm<z.infer<typeof registerFormSchema>>({
+        resolver: zodResolver(registerFormSchema),
         defaultValues: {
             username:"",
             password:"",
         },
     });
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const registerSubmit = (values: z.infer<typeof registerFormSchema>) => {
         console.log(values);
       };
       
     return (
         <Form {...form} >
-            <form onSubmit={form.handleSubmit(onSubmit)} >
+            <form onSubmit={form.handleSubmit(registerSubmit)} >
                 <div className="flex flex-col gap-2 max-w-[300px] m-auto">
                 <CreateUserFormField
                 name="username"
@@ -51,17 +57,59 @@ const CreateUserForm = () => {
         </Form>
       );
     }
+
+    export const LoginUserForm = () => {
+
+        const form = useForm<z.infer<typeof loginFormSchema>>({
+            resolver: zodResolver(loginFormSchema),
+            defaultValues: {
+                username:"",
+                password:"",
+            },
+        });
+        const loggingSubmit = (values: z.infer<typeof loginFormSchema>) => {
+            authService.login(values.username,values.password)
+            .then((promise) => console.log(promise))
+            .catch(() => console.log("blah blah error"))
+          };
+          
+        return (
+            <Form {...form} >
+                <form onSubmit={form.handleSubmit(loggingSubmit)} >
+                    <div className="flex flex-col gap-2 max-w-[300px] m-auto">
+                    <CreateUserFormField
+                    name="username"
+                    label="Username"
+                    placeholder="Username"
+                    inputType="username"
+                    formControl={form.control}
+                    />
+                    <CreateUserFormField 
+                    name="password"
+                    label="Password"
+                    placeholder="Password"
+                    inputType="password"
+                    formControl={form.control}
+                    />
+                    <Button className="max-w-[125px] m-auto">Create User</Button>
+                    </div>
+                </form>
+            </Form>
+          );
+        }
+
+
     
-interface SignupFormFieldProps {
-    name: FieldPath<z.infer<typeof formSchema>>;
+interface formFieldProps {
+    name: FieldPath<z.infer<typeof registerFormSchema>>;
     label: string;
     placeholder: string;
     description?: string;
     inputType?: string;
-    formControl: Control<z.infer<typeof formSchema>, any>
+    formControl: Control<z.infer<typeof registerFormSchema>, any>
 }
 
-const CreateUserFormField: React.FC<SignupFormFieldProps> = ({
+const CreateUserFormField: React.FC<formFieldProps> = ({
     name, label, placeholder, description, inputType, formControl
 }) => {
     return (
@@ -86,7 +134,6 @@ const CreateUserFormField: React.FC<SignupFormFieldProps> = ({
     )
 }
 
-export default CreateUserForm;
 
 
 
