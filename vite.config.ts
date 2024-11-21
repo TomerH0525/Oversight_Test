ac22eac0c5b020804e0b2808c9db4c0cd75824f0
Tomer2H <tomer2b0525@gmail.com>
2024-11-21 18:00:56 +0200

    fix: revrted to old commit , trying to publish site

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Oversight_Test",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
